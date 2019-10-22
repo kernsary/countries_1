be875081270f25fa8470f0a24b7dc945d25f67b7
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-22 14:41:13 +0100

    total population displayed

--- src/app.js
+++ src/app.js
@@ -6,5 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
     data: {
       countries: []
     },
+    mounted(){
+      this.fetchCountries();
+      console.log("countries fetched");
+    },
+    computed: {
+      totalPopulation: function() {
+        return this.countries.reduce((runningTotal, country) => {
+          return runningTotal + country.population;
+        }, 0);
+      }
+    },
+    methods: {
+      fetchCountries: function(){
+        fetch("https://restcountries.eu/rest/v2/all")
+        .then(response => response.json())
+        .then(data => this.countries = data)
+      }
+    }
   })
 })
